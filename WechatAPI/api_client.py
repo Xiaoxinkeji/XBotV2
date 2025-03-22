12427aaf32f0b87562ac4e27a8bab384a7ca948e
renjialu <3264913523@qq.com>
2025-03-22 16:25:04 +0800

    0

---
+++ WechatAPI/api_client.py
@@ -0,0 +1,40 @@
+"""
+WechatAPI客户端主模块
+"""
+
+from loguru import logger
+from .Client.base import BaseMixin
+from .Client.login import LoginMixin
+from .Client.message import MessageMixin
+from .Client.user import UserMixin
+from .Client.chatroom import ChatroomMixin
+from .Client.friend import FriendMixin
+from .Client.hongbao import HongBaoMixin
+from .Client.protect import ProtectMixin
+from .Client.tool import ToolMixin
+
+
+class WechatAPIClient(BaseMixin, 
+                    LoginMixin, 
+                    MessageMixin, 
+                    UserMixin, 
+                    ChatroomMixin, 
+                    FriendMixin, 
+                    HongBaoMixin, 
+                    ProtectMixin, 
+                    ToolMixin):
+    """
+    微信API客户端
+    整合所有功能模块
+    """
+    
+    def __init__(self, host="127.0.0.1", port=9000):
+        """
+        初始化WechatAPI客户端
+        
+        Args:
+            host (str): API服务器地址
+            port (int): API服务器端口
+        """
+        super().__init__(host, port)
+        logger.debug(f"初始化WechatAPI客户端 {host}:{port}") 
